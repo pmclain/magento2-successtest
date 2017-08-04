ff63df854264f32cf4c55e0a0b051a17e88af9e5
pat <pat@localhost.localdomain>
2017-08-04 06:37:29 -0400

    initial commit

---
+++ Plugin/Success.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * Pmclain_SuccessTest extension
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the GPL v3 License
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://www.gnu.org/licenses/gpl.txt
+ *
+ * @category  Pmclain
+ * @package   Pmclain_SuccessTest
+ * @copyright Copyright (c) 2017
+ * @license   https://www.gnu.org/licenses/gpl.txt GPL v3 License
+ */
+namespace Pmclain\SuccessTest\Plugin;
+
+use Magento\Framework\Event\ManagerInterface;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
+use Magento\Checkout\Model\Session;
+use Magento\Sales\Model\OrderFactory;
+use Magento\Sales\Model\Order;
+
+class Success
+{
+  /** @var ManagerInterface */
+  protected $_eventManager;
+
+  /** @var PageFactory */
+  protected $_resultPageFactory;
+
+  /** @var ScopeConfigInterface */
+  protected $_scopeConfig;
+
+  /** @var OrderFactory */
+  protected $_orderFactory;
+
+  /** @var Order */
+  protected $_order;
+
+  /** @var Session */
+  protected $_checkoutSession;
+
+  /**
+   * Success constructor.
+   * @param ManagerInterface $eventManager
+   * @param PageFactory $resultPageFactory
+   * @param ScopeConfigInterface $scopeConfig
+   * @param OrderFactory $orderFactory
+   * @param Session $session
+   */
+  public function __construct(
+    ManagerInterface $eventManager,
+    PageFactory $resultPageFactory,
+    ScopeConfigInterface $scopeConfig,
+    OrderFactory $orderFactory,
+    Session $session
+  ) {
+    $this->_eventManager = $eventManager;
+    $this->_resultPageFactory = $resultPageFactory;
+    $this->_scopeConfig = $scopeConfig;
+    $this->_orderFactory = $orderFactory;
+    $this->_checkoutSession = $session;
+  }
+
+  /**
+   * @param \Magento\Checkout\Controller\Onepage\Success $subject
+   * @param $result
+   * @return \Magento\Framework\View\Result\Page
+   */
+  public function afterExecute(\Magento\Checkout\Controller\Onepage\Success $subject, $result)
+  {
+    if (!$this->_isEnabled()) {
+      return $result;
+    }
+
+    $order = $this->_getTestOrder($subject->getRequest()->getParam('order'));
+
+    if (!$order->getId()) {
+      return $result;
+    }
+
+    $this->_checkoutSession->setLastRealOrderId($order->getIncrementId());
+
+    $resultPage = $this->_resultPageFactory->create();
+
+    $this->_eventManager->dispatch(
+      'checkout_onepage_controller_success_action',
+      ['order_ids' => [$order->getId()]]
+    );
+
+    return $resultPage;
+  }
+
+  /**
+   * @return bool
+   */
+  protected function _isEnabled()
+  {
+    if ($this->_scopeConfig->getValue('dev/debug/success_test', ScopeInterface::SCOPE_STORE)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  /**
+   * @param $incrementId string|bool
+   * @return Order
+   */
+  protected function _getTestOrder($incrementId)
+  {
+    /** @var Order $order */
+    $order = $this->_orderFactory->create();
+
+    $order->loadByIncrementId($incrementId);
+
+    return $order;
+  }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# Magento 2 Checkout Success Page Testing
+
+Test the checkout success page of your Magento 2 store without completing the
+checkout process.
+
+### Installation
+In your Magento 2 root directory run  
+`composer require pmclain/magento2-successtest`  
+`bin/magento setup:upgrade`
+
+### Configuration and Use
+After installation, enable via the Magento admin panel under:  
+`Stores->Confirugation->Advanced->Developer->Debug->Enable Checkout Success Page Testing`  
+You can now navigate to your success page with any increment id include as the
+query parameter `order`, ex http://mage2.dev/checkout/onepage/success/?order=000000008
+
+### License
+GNU GENERAL PUBLIC LICENSE Version 3
